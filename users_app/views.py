9d0aa67b41b9c2bc1843c9e98d7ff1349aaa69e3
Shradha Jain <shraddhajain3131@gmail.com>
2021-06-01 12:45:10 +0530

    update

--- users_app/views.py
+++ users_app/views.py
@@ -8,7 +8,7 @@ def register(request):
         if register_form.is_valid():
             register_form.save()
             messages.success(request, ("New User Account Created, Login To Get Started!"))
-            return redirect('register')
+            return redirect('login')
     else:
         register_form = CustomRegisterForm()
     return render(request, 'register.html', {'register_form': register_form})
